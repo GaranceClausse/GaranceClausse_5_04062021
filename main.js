59f287e0a4a5e4b8d8a3c17cb6ccc9326c187d60
Garance Clausse <garanceclausse@msn.com>
2021-06-25 12:27:06 +0200

    first attempt on JS

--- main.js
+++ main.js
@@ -1,86 +1,25 @@
-class TeddyBear {
 
 
-    constructor(name, price, order) {
- 
- 
-       this.name = name; 
- 
-       this.price = price;
+ for (let teddy of teddies) {
 
-       this.order = order
  
+
+    var img = document.createElement ("img");
+
+    img.className = "card-img-top";
+    img.src = teddy.imageUrl;
+    img.alt= teddy.alt;
+    
+    $("div.card").prepend(img);
+    
+   
+    $("h5.fw-bolder").html("L'ours " + teddy.name);
+    
+
  
-    }
- 
+    $("h1.teddy_name").html("Acheter notre ours : " + teddy.name);
+    $("span.teddy_price").append(teddy.price/100 + "€");
  
  }
 
- let teddyFred = new teddyBear (Fred, 830, 123456789);
- let teddyCharlie = new teddyBear (Charlie, 1020, 234567890);
- let teddyMarc = new teddyBear (Marc, 760, 345678901);
- let teddyJean = new teddyBear (Jean, 960, 456789012);
- let teddyMartin = new teddyBear (Martin, 2030, 567890123);
-
- teddyBought => {
-     console.log ("Merci d'avoir acheté l'ours le plus mignon :" + this.name + " à " + this.price + " ; numéro de commande " + this.order);
- };
-
-teddyFred.teddyBought();
-
-const teddys = [
-
-    {
-        name: "Fred",
-
-        price: 830,
-
-        order: 123456789,
-    },
-
-    {
-        name: "Charlie",
-
-        price: 1020,
-
-        order: 234567890,
-    },
-
-    
-    {
-        name: "Marc",
-
-        price: 760,
-
-        order: 345678901,
-    },
-
-    
-    {
-        name: "Jean",
-
-        price: 960,
-
-        order: 456789012,
-    },
-
-    
-    {
-        name: "Martin",
-
-        price: 2030,
-
-        order: 567890123,
-    }
-]
-
-
-for (let teddy of teddys) {
-
  
-
-    console.log('Achat de votre ours ' + teddy.name + ' ; numéro de commande ' + teddy.order);
- 
-  
- 
- }
