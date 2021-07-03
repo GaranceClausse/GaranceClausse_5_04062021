3c9ea4831d43b55a1804cdb687af2fac210b6f19
Garance Clausse <garanceclausse@msn.com>
2021-07-03 16:15:11 +0200

    tracked url + dynamic page-item

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@
       /* Create image */
       let img = document.createElement ("img");
       /* Set image class +alt */
-        img.className = "card-img-top img-fluid cover h-100 w-auto";
+        img.className = "card-img-top img-fluid cover h-100 w-100";
         img.src = "images/" + teddy.imageUrl;
         img.alt= teddy.alt;
         /* Add image*/  
@@ -56,7 +56,7 @@
         /*Add stars*/
         let stars = document.createElement("div");
         stars.className = "bi-star-fill";
-        $(".reviews" + i).append(stars);
+        $(".reviews" + i).append(stars*5);
 
         /*Add price*/
         let teddy_price =  document.createElement("div");
@@ -74,7 +74,7 @@
         let card_link = document.createElement("a");
         card_link.className = "btn btn-outline-dark mt-auto stretched-link card-link" + i;
         card_link.id= teddy._id;
-        card_link.href= "page-item.html";
+        card_link.href= "page-item.html?"+ teddy._id;
         card_link.innerHTML= "Voir plus";
         $(".card-footer" + i).append(card_link);
 
@@ -88,7 +88,197 @@
 /* Mise en page de page-item */
 
 
-let other_container1 = document.createElement("div");
+      /* Récupération de la chaine de requête dans l'url */
+
+      const queryString_url_id = window.location.search;
+
+      /*extraction id */
+      const realId = queryString_url_id.slice(1);
+
+      
+
+     /* Code HTML de la partie décrivant le produit sélectionné */
+
+     let container_item = document.createElement("div");
+     container_item.className = "container container_page_item px-4 px-lg-5 my-5";
+     $("#container_page_item").append(container_item);
+
+     let sub_container = document.createElement("div");
+     sub_container.className = "row sub_container gx-4 gx-lg-5";
+     $(".container_page_item").append(sub_container);
+
+     let product_pic = document.createElement("div");
+     product_pic.className = "product_pic col-md-6";
+     $(".sub_container").append(product_pic);
+
+     let product_img = document.createElement("img");
+     product_img.className = "card-img-top product_img mb-5 mb-md-0";
+     product_img.src= "images/" + teddy.imageUrl;
+     product_img.alt = teddy.alt;
+     $(".product_pic").append(product_img);
+
+     let product_description = document.createElement("div");
+     product_description.className = "product_description col-md-6";
+     $(".sub_container").append(product_description);
+
+     let product_name = document.createElement("h1");
+     product_name.className = "display-5 fw-bolder teddy_name";
+     product_name.innerHTML = "Votre ours " + teddy.name;
+     $(".product_description").append(product_name);
+
+     let product_price = document.createElement("div");
+     product_price.className = "product_price fs-5 mb-5";
+     product_price.innerHTML = teddy.price/100 + "€";
+     $(".product_description").append(product_price);
+
+     let product_explain = document.createElement("p");
+     product_explain.className = "product_explain lead";
+     product_explain.innerHTML = "Tous nos ours sont faits mains en France par des artisans formés par nos soins. Notre maison existe depuis plus de 200 ans et s'assure de la qualité de chacun de nos produits. Notre charte qualité est à votre disposition en bas de page, ainsi que nos garanties et conditions d'envoie";
+     $(".product_description").append(product_explain);
+
+     let product_form_container = document.createElement("div");
+     product_form_container.className = "product_form_container mb-5";
+     $(".product_description").append(product_form_container);
+
+     let product_form = document.createElement("form");
+     product_form.className = "product_form";
+     $(".product_form_container").append(product_form);
+
+     let product_form_label = document.createElement("label");
+     product_form_label.className = "product_form_label";
+     product_form_label.for = "product_option";
+     product_form_label.innerHTML = "Choisir une option  ";
+     $(".product_form").append(product_form_label);
+
+     let product_form_select = document.createElement("select");
+     product_form_select.className = "product_form_select";
+     product_form_select.name = "product_option";
+     product_form_select.id = "product_option"
+     $(".product_form").append(product_form_select);
+
+     let product_form_option1 = document.createElement("select");
+     product_form_option1.value = "";
+     product_form_option1.innerHTML = teddy.color;
+     $(".product_form_select").append(product_form_option1);
+
+     let product_add_to_cart_container = document.createElement("div");
+     product_add_to_cart_container.className = "d-flex flex-row product_add_to_cart_container";
+     $(".product_description").append(product_add_to_cart_container);
+
+
+     let product_quantity = document.createElement("input");
+     product_quantity.className = "form-control text-center me-3 ps-2 pe-1 product_quantity";
+     product_quantity.id= "inputQuantity";
+     product_quantity.type = "number";
+     product_quantity.min = "0";
+     product_quantity.value = "1";
+     $(".product_add_to_cart_container").append(product_quantity);
+
+     
+     let product_add_to_cart = document.createElement("button");
+     product_add_to_cart.className = "btn btn-outline-dark flex-shrink-0 add-to-cart position-relative product_add_to_cart";
+     product_add_to_cart.name= "btn-order";
+     product_add_to_cart.type = "submit";
+     $(".product_add_to_cart_container").append(product_add_to_cart);
+
+     let cart_icon = document.createElement("i");
+     cart_icon.className = "bi-cart-fill me-1";
+     $(".product_add_to_cart").append(cart_icon);
+
+     let cart_link = document.createElement("a");
+     cart_link.className = "stretched-link cart_link";
+     cart_link.href = "cart.html";
+     cart_link.innerHTML = "Ajouter au panier";
+     $(".product_add_to_cart").append(cart_link);
+
+
+
+     for ( let i=1; i < teddies.length; i++) for (teddy of teddies) {
+      
+
+      let item_container1 = document.createElement("div");
+      item_container1.className= " col-10 col-sm-8 col-md-3 col-lg-2 col-xl-2 mb-5";
+      item_container1.id = "item_container" + i;
+      $("#other_item_container").append(item_container1);
+
+
+      /* Create div */
+      let item_card = document.createElement("div");
+      item_card.className = "card d-flex h-100 justify-content-stretch shadow";
+      item_card.id = "item_card" + i;
+      $("#item_container" + i).append(item_card);
+
+      /*create img container*/
+      let item_img_cont = document.createElement("div");
+        item_img_cont.className = "w-100 d-flex justify-content-center item_img_cont" + i;
+        $("#item_card" + i).append(item_img_cont);
+
+      /* Create image */
+      let item_img = document.createElement ("img");
+      /* Set image class +alt */
+      item_img.className = "card-img-top img-fluid cover h-100 w-100";
+      item_img.src = "images/" + teddy.imageUrl;
+      item_img.alt= teddy.alt;
+        /* Add image*/  
+        $(".item_img_cont" + i).prepend(item_img);
+
+        /* Create card_body */
+
+        let item_card_body = document.createElement("div");
+        item_card_body.className = "p-4 h-100  d-flex flex-column item_card-body" + i;
+        $("#item_card" + i).append(item_card_body);
+
+        /* Create text-center */
+
+        let item_text_center = document.createElement("div");
+        item_text_center.className = "text-center h-100 d-flex flex-column item_title" + i;
+        $(".item_card-body" + i).append(item_text_center);
+
+        /* Create Teddy's name */
+        let item_teddy_name =  document.createElement("h5");
+        item_teddy_name.className = "fw-bolder mb-auto item_teddy_name" + i;
+        item_teddy_name.innerHTML = "L'ours " + teddy.name;
+        $(".item_title" + i).append(item_teddy_name);
+
+        /* Create reviews */
+        let item_reviews = document.createElement("div");
+        item_reviews.className = "d-flex justify-content-center justify-self-end p-3 mb-0 small text-warning item_reviews" + i;
+        $(".item_title" + i).append(item_reviews);
+
+        /*Add stars*/
+        let item_stars = document.createElement("div");
+        item_stars.className = "bi-star-fill";
+        $(".item_reviews" + i).append(item_stars*5);
+
+        /*Add price*/
+        let item_teddy_price =  document.createElement("div");
+        item_teddy_price.className = "fw-bolder mb-0 item_teddy_price" + i;
+        item_teddy_price.innerHTML = teddy.price/100 + " €";
+        $(".item_title" + i).append(item_teddy_price);
+
+        /*Add footer card */
+
+        let item_card_footer = document.createElement("div");
+        item_card_footer.className = "d-flex flex-row p-2 pb-3 border-bottom-1 mb-0 mt-auto bg-transparent align-self-center item_card-footer" + i;  
+        $("#item_card" + i).append(item_card_footer);
+
+        /* Add link */
+        let item_card_link = document.createElement("a");
+        item_card_link.className = "btn btn-outline-dark mt-auto stretched-link item_card-link" + i;
+        item_card_link.id= teddy._id;
+        item_card_link.href= "page-item.html?"+ teddy._id;
+        item_card_link.innerHTML= "Voir plus";
+        $(".item_card-footer" + i).append(item_card_link);
+
+        
+      i++;
+
+      }
+
+
+     
+
+/*let other_container1 = document.createElement("div");
 other_container1.className= "row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4";
 other_container1.id = "other-container";
 $("#other_item_container").append(other_container1);
@@ -102,24 +292,24 @@ for ( let i=1; i < teddies.length; i++) for (teddy of teddies) {
   $("#other-container").append(other_container2);
 
 
-  /* Create div */
+  /* Create div 
   let other_card = document.createElement("div");
   other_card.className = "card d-flex h-100 justify-content-stretch shadow";
   other_card.id = "other-card" + i;
   $("#container2" + i).append(other_card);
 
-  /*create img container*/
+  /*create img container
   let other_img_cont = document.createElement("div");
     other_img_cont.className = "w-100 d-flex justify-content-center other_img_cont" + i;
     $("#other-card" + i).append(other_img_cont);
 
-  /* Create image */
+  /* Create image 
   let img = document.createElement ("img");
-  /* Set image class +alt */
+  /* Set image class +alt 
     img.className = "card-img-top img-fluid cover h-100 w-auto";
     img.src = "images/" + teddy.imageUrl;
     img.alt= teddy.alt;
-    /* Add image*/  
+    /* Add image
     $(".other_img_cont" + i).prepend(img);
 
     
